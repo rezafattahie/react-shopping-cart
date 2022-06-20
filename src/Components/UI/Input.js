fe338208dae38cecc9db9edcd6bf716ae6547933
Reza Fattahie <rezafatahy@gmail.com>
2022-06-20 14:56:48 +0430

    Update Input.js

--- src/Components/UI/Input.js
+++ src/Components/UI/Input.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import classes from './Input.module.css';
+
+const Input = React.forwardRef((props, ref) => {
+  return (
+    <input className={classes.input} ref={ref} type="Number" {...props.input} />
+  );
+});
+export default Input;
 
